f0d0ce197f2f7ed74e782ab8409c2f572ca3d609
Berat Uzun <beratuzun@MacBook-Air-von-Berat.local>
2025-12-21 23:55:36 +0100

    fix/labels

--- apps/frontend/src/components/forms/times-form.tsx
+++ apps/frontend/src/components/forms/times-form.tsx
@@ -1,3 +1,4 @@
+import { Field } from "../ui/field";
 import { Input } from "../ui/input";
 
 type TimesFormProps = {
@@ -15,22 +16,28 @@ const TimesForm = ({
 }: TimesFormProps) => {
   return (
     <form className="mt-4 h-full justify-end flex mb-4 items-end p-3 gap-6">
-      <Input
-        type="time"
-        id="start-time-picker"
-        required
-        defaultValue={start_time}
-        onChange={(e) => setStart(e.target.value)}
-        className="h-20 text-xl [&>svg]:hidden"
-      />
-      <Input
-        type="time"
-        id="end-time-picker"
-        required
-        defaultValue={end_time}
-        onChange={(e) => setEnd(e.target.value)}
-        className="h-20 text-xl [&>svg]:hidden"
-      />
+      <div className="flex flex-col flex-1 gap-2">
+        <Field className="text-xl ml-1">Von</Field>
+        <Input
+          type="time"
+          id="start-time-picker"
+          required
+          defaultValue={start_time}
+          onChange={(e) => setStart(e.target.value)}
+          className="h-20 text-xl [&>svg]:hidden"
+        />
+      </div>
+      <div className="flex flex-col flex-1 gap-2">
+        <Field className="text-xl ml-1">Bis</Field>
+        <Input
+          type="time"
+          id="end-time-picker"
+          required
+          defaultValue={end_time}
+          onChange={(e) => setEnd(e.target.value)}
+          className="h-20 text-xl [&>svg]:hidden"
+        />
+      </div>
     </form>
   );
 };
