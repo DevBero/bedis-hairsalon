f59e2fa2e7dc5c6d6aed5919cc9fd9760f339914
Berat Uzun <beratuzun@MacBook-Air-von-Berat.local>
2025-12-29 01:24:04 +0100

    fix/debug times form

--- apps/frontend/src/components/forms/times-form.tsx
+++ apps/frontend/src/components/forms/times-form.tsx
@@ -1,4 +1,4 @@
-// import { Input } from "../ui/input";
+import { Input } from "../ui/input";
 
 type TimesFormProps = {
   start_time: string;
@@ -14,25 +14,25 @@ const TimesForm = ({
   setEnd,
 }: TimesFormProps) => {
   return (
-    <form className="mt-4 h-full mb-6 p-3 flex flex-col justify-end gap-6 overflow-x-hidden border border-red-500">
-      <div className="flex flex-col gap-2 min-w-0 w-full border border-orange-500">
-        <input
+    <form className="mt-4 h-full mb-6 p-3 flex flex-col justify-end gap-6 overflow-x-hidden">
+      <div className="flex flex-col gap-2 min-w-0 w-full">
+        <Input
           type="time"
           id="start-time-picker"
           required
           defaultValue={start_time}
           onChange={(e) => setStart(e.target.value)}
-          className="h-20 text-xl [&>svg]:hidden w-full border-green-500"
+          className="h-20 text-xl [&>svg]:hidden"
         />
       </div>
       <div className="flex flex-col gap-2 min-w-0 w-full">
-        <input
+        <Input
           type="time"
           id="end-time-picker"
           required
           defaultValue={end_time}
           onChange={(e) => setEnd(e.target.value)}
-          className="h-20 text-xl [&>svg]:hidden w-full"
+          className="h-20 text-xl [&>svg]:hidden"
         />
       </div>
     </form>
